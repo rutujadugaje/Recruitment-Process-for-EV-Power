ee32c805ecdd6e56be5c8142ff658fc54a31ebc6
sandip <sandipbaste999@gmail.com>
2025-10-27 15:36:22 +0530

    cut feader footer in OnlineTest

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from './Components/Layout';
 import HomeSection from './Components/HomeSection';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import OnlineTest from './Components/OnlineTest';
 import About from './Components/About';
 import EmployeeBenefits from './Components/EmployeeBenefits';
@@ -16,38 +16,56 @@ import SignUp from './Components/SignUp';
 import Login from './Components/Login';
 import InterviewTest from './Components/InterviewTest';
 import JobDetails from './Components/JobDetails';
-import HRDashboard from './Components/HRDashboard'
-import AdminDasboard from './Components/AdminDashboard'
+import HRDashboard from './Components/HRDashboard';
 import AdminDashboard from './Components/AdminDashboard';
 
-const App = () => {
+// Component to conditionally render Layout and Footer
+const AppContent = () => {
+  const location = useLocation();
+  
+  // Routes where we don't want to show header and footer
+  const noHeaderFooterRoutes = [
+    '/onlinetest',
+    '/user-aptitude-login',
+    '/interviewtest',
+    '/hr-dashboard',
+    '/admindashboard'
+  ];
+  
+  const shouldShowHeaderFooter = !noHeaderFooterRoutes.includes(location.pathname);
+
   return (
     <div>
-      <BrowserRouter>
-        <Layout />
-        <Routes>
-          <Route exact path="/" element={<HomeSection />} />
-          <Route exact path="/onlinetest" element={<OnlineTest />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/emp-benefits" element={<EmployeeBenefits />} />
-          <Route exact path="/form-respones" element={<FormResponce />} />
-          <Route exact path="/job-positions" element={<JobPositions />} />
-          <Route path="/experience-form/:title/:details/:location" element={<ExperienceForm />} />
-          <Route path="/user-aptitude-login" element={<AptitudeLogin />} />
-          <Route path="/test-history" element={<UserTestsTable />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route exact path="/interviewtest" element={<InterviewTest />} />
-          <Route exact path="/job-details" element={<JobDetails />} />
-          <Route exact path="/hr-dashboard" element={<HRDashboard />} />
-          <Route exact path="/admindashboard" element={<AdminDashboard />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {shouldShowHeaderFooter && <Layout />}
+      <Routes>
+        <Route exact path="/" element={<HomeSection />} />
+        <Route exact path="/onlinetest" element={<OnlineTest />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/emp-benefits" element={<EmployeeBenefits />} />
+        <Route exact path="/form-respones" element={<FormResponce />} />
+        <Route exact path="/job-positions" element={<JobPositions />} />
+        <Route path="/experience-form/:title/:details/:location" element={<ExperienceForm />} />
+        <Route path="/user-aptitude-login" element={<AptitudeLogin />} />
+        <Route path="/test-history" element={<UserTestsTable />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route exact path="/interviewtest" element={<InterviewTest />} />
+        <Route exact path="/job-details" element={<JobDetails />} />
+        <Route exact path="/hr-dashboard" element={<HRDashboard />} />
+        <Route exact path="/admindashboard" element={<AdminDashboard />} />
+      </Routes>
+      {shouldShowHeaderFooter && <Footer />}
     </div>
   );
 };
 
-export default App;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
 
+export default App;
